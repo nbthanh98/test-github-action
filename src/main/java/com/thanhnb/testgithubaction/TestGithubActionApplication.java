b2c10a99747b43356b3f1544e853e62ebafc1750
nbthanh98 <nbthanh98>
2023-12-04 00:28:07 +0700

    ud

---
+++ src/main/java/com/thanhnb/testgithubaction/TestGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.thanhnb.testgithubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGithubActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thanhnb/testgithubaction/TestGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhnb.testgithubaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGithubActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
